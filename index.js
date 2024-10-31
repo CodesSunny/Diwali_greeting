fa23a41783026def9aaf6342685bc05ee55c9828
TechSunny <sunnymotivation2@gmail.com>
2024-10-31 16:29:24 +0530

    improved ui

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ window.onload =()=>{
     const receiverName = document.querySelector("#receiver");
     const senderName = document.querySelector("#sender");
     receiverName.innerText = new URLSearchParams(location.search).get(
-      "receiver") ||"receiver"
+      "receiver") ||"Receiver"
     ;
     senderName.innerText = new URLSearchParams(location.search).get("sender") || 'Sunny';  
 }
