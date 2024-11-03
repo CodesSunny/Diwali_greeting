a1f46f30a27498900117cfe3b188c9939abc78c8
TechSunny <sunnymotivation2@gmail.com>
2024-11-03 22:14:38 +0530

    added breakpoints for responsiveness

---
+++ gsap.js
@@ -0,0 +1,25 @@
+window.onload=()=>{
+    gsap.registerPlugin(Flip);
+    gsap.registerPlugin(MotionPathPlugin);
+
+    const squares = gsap.utils.toArray(".square");
+    
+    function doFlip() {
+      // Get the initial state
+      const state = Flip.getState(squares);
+
+      // Make DOM or styling changes (swap the squares in our case)
+      swap(squares);
+      
+      // Animate from the initial state to the end state
+      Flip.from(state, {duration: 2, ease: "power1.inOut"});
+    }
+    
+    // Given an Array of two siblings, append the one that's first so it's last (swap)
+    function swap([a, b]) {
+      a.parentNode.children[0] === a ? a.parentNode.appendChild(a) : a.parentNode.appendChild(b);
+    }
+    
+    // click to flip
+    document.querySelector(".button").addEventListener("click", doFlip);
+    }
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-window.onload =()=>{
+
     const receiverName = document.querySelector("#receiver");
     const senderName = document.querySelector("#sender");
     receiverName.innerText = new URLSearchParams(location.search).get(
       "receiver") ||"Receiver"
     ;
     senderName.innerText = new URLSearchParams(location.search).get("sender") || 'Sunny';  
-}
+
